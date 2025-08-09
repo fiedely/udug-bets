d338d6f59175e0e8d417c014ee525032047eede2
fiedely <delz7777@gmail.com>
2025-08-09 07:48:12 +0700

    set the correct globaloptions deployment for all cloud functions

--- functions/src/callable.ts
+++ functions/src/callable.ts
@@ -1,8 +1,11 @@
 // functions/src/callable.ts
 
 import * as logger from "firebase-functions/logger";
+import { setGlobalOptions } from "firebase-functions/v2";
 import { HttpsError, onCall } from "firebase-functions/v2/https";
 import { db, Tournament, UserProfile } from "./common";
+
+setGlobalOptions({ region: "asia-southeast2" });
 
 export const deleteTournamentAndData = onCall(async (request) => {
     const uid = request.auth?.uid;
